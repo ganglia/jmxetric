ganglia/jmxetric

commit 78fb5c7566a22c9b30eaeeb1198c85e2e6d0297e

add default constants for XMLConfigurationService

src/main/java/info/ganglia/jmxetric/XMLConfigurationService.java
@@ -25,8 +25,11 @@ public class XMLConfigurationService {
 
     private final static XPath xpath = XPathFactory.newInstance().newXPath();
     
+    private static final String DEFAULT_HOST = "localhost";
+    private static final String DEFAULT_PORT = "8649";
     private static final String DEFAULT_CONFIG="jmxetric.xml";
     private static final String DEFAULT_MODE="multicast";
+    private static final String DEFAULT_USE_WIREFORMAT31 = "false";
     
     /**
      * Configures the JMXetricAgent based on the supplied agent args
@@ -45,11 +48,11 @@ public class XMLConfigurationService {
         
         if ( agentArgs != null ) {
             String[] args = agentArgs.split(",");
-            host = getTagValue( "host", args, null );
-            port = getTagValue( "port", args, null );
-            config = getTagValue( "config", args, DEFAULT_CONFIG );
-            mode = getTagValue( "mode", args, null );
-            wireformat = getTagValue( "wireformat31x", args, null);
+            host = getTagValue("host", args, DEFAULT_HOST);
+            port = getTagValue("port", args, DEFAULT_PORT);
+            config = getTagValue("config", args, DEFAULT_CONFIG);
+            mode = getTagValue( "mode", args, DEFAULT_MODE);
+            wireformat = getTagValue( "wireformat31x", args, DEFAULT_USE_WIREFORMAT31);
             processName = getTagValue( "process", args, null );
             spoof = getTagValue( "spoof", args, null );
         }

commit 38c21916973608b9c4b88b08182315363be16252

Fix regression introduced by 78fb5c7
The commit introduced default values for configuration parameters that
can be specified on the command line, but it set those default values
too early.
This led to it overwriting those values when they are specified in the
XML file.
This change changes it so that the absence of a command line parameter
leads to it being set as null, rather than the default value.
The default value is set during the last stage of the Ganglia
configuration, which is after the XML file has been read.

src/main/java/info/ganglia/jmxetric/XMLConfigurationService.java
@@ -42,21 +42,21 @@ public class XMLConfigurationService {
      * @throws java.lang.Exception
      */
     public static void configure(JMXetricAgent agent, String agentArgs) throws Exception {
-        String host = null ;
-        String port = null ;
-        String mode = null ;
-        String config = DEFAULT_CONFIG ;
-        String wireformat = null ;
-        String processName = null ;
-        String spoof = null;
+    	String host = null,
+    		   port = null,
+    		   mode = null,
+    		   config = DEFAULT_CONFIG,
+    		   wireformat = null,
+    		   processName = null,
+    		   spoof = null;
         
-        if ( agentArgs != null ) {
+        if (agentArgs != null) {
             String[] args = agentArgs.split(",");
-            host = getTagValue("host", args, DEFAULT_HOST);
-            port = getTagValue("port", args, DEFAULT_PORT);
+            host = getTagValue("host", args, null);
+            port = getTagValue("port", args, null);
             config = getTagValue("config", args, DEFAULT_CONFIG);
-            mode = getTagValue( "mode", args, DEFAULT_MODE);
-            wireformat = getTagValue( "wireformat31x", args, DEFAULT_USE_WIREFORMAT31);
+            mode = getTagValue( "mode", args, null);
+            wireformat = getTagValue( "wireformat31x", args, null);
             processName = getTagValue( "process", args, null );
             spoof = getTagValue( "spoof", args, null );
         }
@@ -155,9 +155,9 @@ public class XMLConfigurationService {
         Node g = (Node) xpath.evaluate(gangliaExpr, inputSource,
                 XPathConstants.NODE);
         String hostname = selectParameterFromNode(cmdLineHost,
-        		g, "hostname", "localhost");
+        		g, "hostname", DEFAULT_HOST);
         String port = selectParameterFromNode(cmdLinePort,
-        		g, "port", "8649");
+        		g, "port", DEFAULT_PORT);
         int iport = Integer.parseInt(port);
         String mode = selectParameterFromNode(cmdLineMode,
         		g, "mode",DEFAULT_MODE);
@@ -166,7 +166,7 @@ public class XMLConfigurationService {
         	addressingMode = UDPAddressingMode.UNICAST;
         }
         String stringv31x = selectParameterFromNode(cmdLinev31x,
-        		g, "wireformat31x", "false");
+        		g, "wireformat31x", DEFAULT_USE_WIREFORMAT31);
         boolean v31x = Boolean.parseBoolean(stringv31x);
         String spoof = selectParameterFromNode(cmdLineSpoof,
                 g, "spoof", null);